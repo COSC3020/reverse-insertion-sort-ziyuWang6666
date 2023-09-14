f0cdbae1fee607c8e0ee51b47bec27ff2eac0133
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-09-13 21:07:36 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
-function insertionSortReverse(array) {
-    return array;
+function insertionSortReverse(arr) {
+    for(var i = arr.length - 1; i >= 0; i--) {
+        var val = arr[i];
+        var j;
+        for(j = i; j < arr.length && arr[j+1] < val;j++) {
+            arr[j] = arr[j+1];
+        }
+        arr[j]= val;
+    }
+    return arr;
 }
+console.log(insertionSortReverse([10,20,4,-1,5,9,3,-190,40,21]));
+// get some help from TA
